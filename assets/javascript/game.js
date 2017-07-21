3b2e9a9714db8cb7ed43410dc9f8bc5a81ec86ee
Javier Avitia <Javier.A.Avitia@gmail.com>
2017-07-20 23:20:04 -0700

    Basic functionality achieved.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,11 @@
 
 var variables = {
 	guesses: [],
+	remaining:15,
 	valid: "abcdefghijklmnopqrstuvwxyz",
 	words: ["kitten","doggo","snakeroo","kitten","penguin","kitten"],
+	counter: 0,
+	wins:0,
 	word: function() {
 		return this.words[Math.floor(Math.random()*this.words.length)];
 		},
@@ -15,6 +18,7 @@ var variables = {
 		        }
 		    }
 		    console.log(indexes);
+		    return indexes;
 		},
 	hidden: function(word){
 			var hidden = ""
@@ -24,10 +28,7 @@ var variables = {
 			}
 			return hidden;
 		}
-
-	//["kitten","doggo","snakeroo"]
-	//computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-}
+};
 
 /*function indexArr(arr, val){
 	var indexes=[];
@@ -50,24 +51,57 @@ var variables = {
 	console.log(hidden);
 };*/
 
+var the_word = variables.word();
+var hidden_word = variables.hidden(the_word);
 
 document.onkeyup = function(event) {
+	//variables.counter++;
+	variables.remaining--;
+	variables.wins++;
+
 	var key = event.key;
 	var $guesses = document.getElementById("guesses");
-	var $word = document.getElementById("word")
-	var check = variables.guesses.indexOf(key)
+	var $word = document.getElementById("word");
+	var $remaining = document.getElementById("remaining");
+	var $wins = document.getElementById("wins");
+	var check = variables.guesses.indexOf(key);
+	var check_guess = the_word.indexOf(key);
 	//may need to comment out below
-	var the_word = variables.word();
+	//var the_word = variables.word();
+
+	//var hidden_word = variables.hidden(the_word);
 
 	if (check === -1){
 		variables.guesses.push(key);
 	}
+
+	if (check_guess > -1){
+		var correctArr = variables.indexArr(the_word,key);
+		var hiddenArr = hidden_word.split("");
+		for (k=0;k<correctArr.length;k++){
+			hiddenArr[correctArr[k]] = hiddenArr[correctArr[k]].replace(/-/g,key);
+			console.log(hiddenArr);
+			console.log(correctArr[k]);
+		};
+		hidden_word = hiddenArr.join("");
+		//console.log(hidden_word);
+		//$word.innerHTML = hidden_word;
+	}
+
+	if (variables.remaining === 0){
+		alert("You lost!")
+	}
 	
 	$guesses.innerHTML = variables.guesses;
-	$word.innerHTML = variables.hidden(the_word);
+	//$word.innerHTML = variables.hidden(the_word);
+	$word.innerHTML = hidden_word;
+	$wins.innerHTML = variables.wins;
+	$remaining.innerHTML = variables.remaining;
 
+	//working for arrays looking for words
+	//variables.indexArr(variables.words,"kitten");
 
-	variables.indexArr(variables.words,"kitten");
+	//variables.indexArr(the_word,key);
 	console.log(the_word);
-	//variables.hidden(the_word);
+	variables.hidden(the_word);
 }
